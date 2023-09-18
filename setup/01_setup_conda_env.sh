2240bc1bb2987da583837c4288c5195dd48bca7b
Hugh Saalmans <hugh.saalmans@iag.com.au>
2023-09-18 15:26:13 +1000

    working on PosGIS notebook

--- setup/01_setup_conda_env.sh
+++ setup/01_setup_conda_env.sh
@@ -5,7 +5,7 @@
 # --------------------------------------------------------------------------------------------------------------------
 
 ENV_NAME=geoworkshop
-PYTHON_VERSION="3.10"
+PYTHON_VERSION="3.11"
 
 # Note: If you want to install QGIS via Conda - it only supports up to Python 3.9
 
@@ -37,7 +37,10 @@ conda config --env --set channel_priority strict
 conda activate ${ENV_NAME}
 
 # install geospatial packages and restart env
-conda install -y -c conda-forge leafmap geopandas localtileserver keplergl pydeck sqlalchemy psycopg jupyter_contrib_nbextensions
+conda install -y geopandas
+conda install -y -c conda-forge mamba
+mamba install -y -c conda-forge leafmap sqlalchemy psycopg2
+#conda install -y -c conda-forge leafmap geopandas localtileserver keplergl pydeck sqlalchemy psycopg jupyter_contrib_nbextensions
 conda activate ${ENV_NAME}
 
 ## additional package requiring pip
